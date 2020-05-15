9bb474772c9b87a54e38a0e3b9c1882480c4606e
Mariya <mariya.podosinova@gmail.com>
2020-05-15 09:13:22 +0100

    Choice random color with a click on the box

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
+import { choice } from "./helpers";
 import "./Card.css";
 
 class Card extends Component {
+  static defaultProps = {
+    allCard: ["#010", "#020", "#030", "#040", "#050"],
+  };
   constructor(props) {
     super(props);
-    this.state = { color: "purple" };
+    this.state = { color: choice(this.props.allCard) };
+    this.handleCLick = this.handleCLick.bind(this);
   }
+
+  pickColor() {
+    let newColor;
+    do {
+      newColor = choice(this.props.allCard);
+    } while (newColor === this.state.color);
+
+    this.setState({ color: newColor });
+  }
+  handleCLick() {
+    this.pickColor();
+  }
+
   render() {
     return (
       <div
