287e8bec6a8684188f64ee779a1b6d541bab72e3
Mariya <mariya.podosinova@gmail.com>
2020-05-15 09:49:00 +0100

    Updates

---
+++ precache-manifest.5c387310d40342b83e6728dc979bc03e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e3eeb5624d5f9e0f9f6255e50e96cd",
+    "url": "/MariyaLcs/color_boxes/index.html"
+  },
+  {
+    "revision": "be2da14efdfb3d34918d",
+    "url": "/MariyaLcs/color_boxes/static/css/main.01b69597.chunk.css"
+  },
+  {
+    "revision": "283fb817e6ded168d2f6",
+    "url": "/MariyaLcs/color_boxes/static/js/2.3d4f4fd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MariyaLcs/color_boxes/static/js/2.3d4f4fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2da14efdfb3d34918d",
+    "url": "/MariyaLcs/color_boxes/static/js/main.bfe217bf.chunk.js"
+  },
+  {
+    "revision": "01f0b07ec05082f391cd",
+    "url": "/MariyaLcs/color_boxes/static/js/runtime-main.27f8ad4a.js"
+  }
+]);
